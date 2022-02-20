39fc98ddca011ac251d9c40cc11a48a45fdf80e3
DeVonte Ewell <devonteewell@DeVontes-MacBook-Pro.local>
2022-02-20 09:47:22 -0800

    updated winning conditions

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ window.addEventListener('DOMContentLoaded', () => {
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
+        [0, 3, 6],
         [1, 4, 7],
         [2, 5, 8],
         [0, 4, 8],
